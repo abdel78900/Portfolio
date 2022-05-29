00054379cfff194c4c1169296c2756554201cb29
abdel78900 <vmnagmb@gmail.com>
2022-05-29 09:27:29 +0200

    finish project

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,50 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_xekr4dr', 'template_07ncne7', form.current, 'tprVX8Irv9bUelFfY')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+          e.target.reset()
+      };
+
     return (
         <section id='contact'>
-           Contact 
+           <h5>Get in Touch</h5>
+           <h2>Contact Me</h2>
+           <div className="container contact__container">
+               <div className="contact__options">
+                   <article className="contact__option">
+                       <MdOutlineEmail className='contact__option-icon'/>
+                      <h4>Email</h4>
+                   <h5>abdel78900@hotmail.com</h5>               
+                   <a href="mailto:abdel78900@hotmail.com">Send a message</a>  
+                   </article>
+                   <article className="contact__option">
+                       <BsWhatsapp className='contact__option-icon'/>
+                      <h4>WhatsApp</h4>
+                   <h5>06 85 38 84 41</h5>               
+                   <a href="https://api.whatsapp.com/send?phone=33685388441" target='_blank'>Send a message</a>  
+                   </article>
+                   </div>
+                   <form ref={form} onSubmit={sendEmail}>
+                       <input type="text" name='name' placeholder='Your Full Name' required />
+                       <input type="text" name='email' placeholder='Your Email' required />
+                       <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+                       <button type="submit" className='btn btn-primary'>Send Message</button>
+                   </form>
+           </div>
         </section>
     )
 }
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 import './testimonials.css'
+import IMG1 from '../../assets/imgportfolio.webp'
+import { Pagination} from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 
 const Testimonials = () => {
     return (
-        <section>
-           Testimonials 
+        <section id='testimonials'>
+           <h5>Review from clients</h5>
+           <h2>Testimonials</h2>
+           <Swiper className="container testimonials__container"
+           modules={[Pagination]}
+           spaceBetween={40}
+           slidesPerView={1}
+           pagination={{ clickable: true }}
+         
+           >
+               <SwiperSlide className="testimonial">
+                   <div className="client__avatar">
+                       <img src={IMG1} alt="Avatar one" />
+                   </div>
+                       <h5 className="client__name">name test</h5>
+                       <small className="client__review">
+                           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis saepe in molestiae aliquid amet exercitationem nihil et fuga! Quidem, ab!
+                       </small>
+               </SwiperSlide>
+               <SwiperSlide className="testimonial">
+                   <div className="client__avatar">
+                       <img src={IMG1} alt="Avatar one" />
+                   </div>
+                       <h5 className="client__name">name test</h5>
+                       <small className="client__review">
+                           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis saepe in molestiae aliquid amet exercitationem nihil et fuga! Quidem, ab!
+                       </small>
+               </SwiperSlide>
+               <SwiperSlide className="testimonial">
+                   <div className="client__avatar">
+                       <img src={IMG1} alt="Avatar one" />
+                   </div>
+                       <h5 className="client__name">name test</h5>
+                       <small className="client__review">
+                           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis saepe in molestiae aliquid amet exercitationem nihil et fuga! Quidem, ab!
+                       </small>
+               </SwiperSlide>
+               
+           </Swiper>
         </section>
     )
 }
